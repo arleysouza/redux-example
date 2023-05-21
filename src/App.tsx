92990a15775a9aef14aad5f120291b98e027fa6d
arleysouza <arley.ferreira.souza@gmail.com>
2023-05-21 18:03:26 -0300

    exemplo

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useDispatch, useSelector } from "react-redux";
+import { set, setA, setB } from "./features/slice";
+import { AppDispatch, RootState } from "./features/store";
+
 
 function App() {
+  const dispatch = useDispatch<AppDispatch>();
+  const a = useSelector( (state:RootState) => state.a);
+  const b = useSelector( (state:RootState) => state.b);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div>A:{a} B:{b}</div>
+      <button onClick={()=>dispatch(setA(2))}>setA</button>
+      <button onClick={()=>dispatch(setB(2))}>setB</button>
+      <button onClick={()=>dispatch(set(3))}>set</button>
     </div>
   );
 }
